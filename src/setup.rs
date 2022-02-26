fb7b635d5ca75d5a7a58da6a2925805a29829098
ProspectPyxis <prospectpyxis@gmail.com>
2022-02-26 17:59:06 +0700

    initial commit

---
+++ src/setup.rs
@@ -0,0 +1,14 @@
+use bevy::prelude::*;
+
+pub struct GameSetup;
+
+impl Plugin for GameSetup {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(WindowDescriptor {
+            title: "Lich thing".to_string(),
+            width: 640.0,
+            height: 480.0,
+        })
+        .add_plugins(DefaultPlugins);
+    }
+}
